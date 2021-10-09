5cd125756c0e567b957d93ed816b452d7926400c
profeInfoAtenea <90715116+profeInfoAtenea@users.noreply.github.com>
2021-10-09 15:50:37 +0200

    Update tiempos_calculo_factorial.py

--- tiempos_calculo_factorial.py
+++ tiempos_calculo_factorial.py
@@ -20,3 +20,36 @@ def factorial_rec(n):
         return n * factorial_rec(n-1)
 #
 
+#
+N = 1416
+'''
+# El valor obtenido se corresponde con los segundos
+# que han transcurrido desde el comienzo de la época:
+'''
+# 1 de enero de 1970, 0 hora
+print("Han trascurrido ", time.time(), " segundos desde el 1 de enero de 1970 #Función: Time.Time()")
+'''
+#
+'''
+print("Cálculo de factorial(",N,") ")
+n = N
+inicio = time.time()
+res_fact_while = factorial_while(n)
+fin = time.time()
+total = fin - inicio
+print("Tiempo factorial_while: ",total)
+
+inicio = time.time()
+res_fact_for = factorial_for(n)
+fin = time.time()
+total = fin - inicio
+print("Tiempo factorial_for: ",total)
+
+inicio = time.time()
+res_fact_rec = factorial_rec(n)
+fin = time.time()
+total = fin - inicio
+print("Tiempo factorial_rec: ",total)
+
+
+print("res_fact_while:",res_fact_while,", res_fact_for:",res_fact_for, "res_fact_rec:",res_fact_rec)
